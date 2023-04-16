6d2979b8ee740557490e56202fabd8de86c6f84a
Moein Barzegaran <barzegaranmoein@gmail.com>
2023-04-17 01:23:29 +0300

    Adding custom navigation bar to the leadership view

--- LeaderboardProject/Scenes/LeadershipBoardScreen/LeadershipBoardViewController.swift
+++ LeaderboardProject/Scenes/LeadershipBoardScreen/LeadershipBoardViewController.swift
@@ -2,16 +2,37 @@ import UIKit
 
 class LeadershipBoardViewController: UIViewController {
 
+    private lazy var navigationbar: LeadershipBoardNavBar = {
+        let navbar = LeadershipBoardNavBar()
+        navbar.translatesAutoresizingMaskIntoConstraints = false
+        return navbar
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        addBackgroundGradientLayer()
+        setupViews()
     }
 }
 
 // MARK: Private methods
 
 private extension LeadershipBoardViewController {
+    func setupViews() {
+        addBackgroundGradientLayer()
+        addNavigationView()
+    }
+    
+    func addNavigationView() {
+        view.addSubview(navigationbar)
+        
+        NSLayoutConstraint.activate([
+            navigationbar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            navigationbar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            navigationbar.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
     func addBackgroundGradientLayer() {
         let gradientLayer = CAGradientLayer()
         gradientLayer.colors = [UIColor.gradientTop,
---
+++ LeaderboardProject/Scenes/LeadershipBoardScreen/Subviews/LeadershipBoardNavBar.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+class LeadershipBoardNavBar: UIView {
+    
+    private lazy var profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = .red
+        return imageView
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.text = "Leaderboard"
+        label.font = .systemFont(ofSize: 16, weight: .bold)
+        label.textColor = .white
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        profileImageView.layer.cornerRadius = profileImageView.frame.width / 2
+    }
+}
+
+// MARK: Private methods
+
+private extension LeadershipBoardNavBar {
+    func setupViews() {
+        addSubview(profileImageView)
+        addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            profileImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            profileImageView.topAnchor.constraint(equalTo: topAnchor),
+            profileImageView.heightAnchor.constraint(equalToConstant: 40),
+            profileImageView.widthAnchor.constraint(equalTo: profileImageView.heightAnchor),
+            
+            titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: 12)
+        ])
+    }
+}
