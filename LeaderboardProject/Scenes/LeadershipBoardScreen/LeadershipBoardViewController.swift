597f362cc7d7e1986985c969b83518936c53d996
Moein Barzegaran <barzegaranmoein@gmail.com>
2023-04-16 03:34:46 +0300

    Changing folder structure to make it more clean

--- LeaderboardProject/Scenes/LeadershipBoardScreen/LeadershipBoardViewController.swift
+++ LeaderboardProject/Scenes/LeadershipBoardScreen/LeadershipBoardViewController.swift
@@ -1,12 +1,10 @@
 import UIKit
 
-class ViewController: UIViewController {
+class LeadershipBoardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
     }
-
-
 }
 
