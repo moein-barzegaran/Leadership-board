74c88f56af396d8cb34ef9790042c0ad48a5d4c4
Moein Barzegaran <barzegaranmoein@gmail.com>
2023-04-16 04:00:51 +0300

    Add background gradient color

---
+++ LeaderboardProject/Foundation/Extensions/Color+Extensions.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+extension UIColor {
+    static let gradientTop = UIColor(named: "gradientTop")!
+    static let gradientCenter = UIColor(named: "gradientCenter")!
+    static let gradientBottom = UIColor(named: "gradientBottom")!
+}
--- LeaderboardProject/Scenes/LeadershipBoardScreen/LeadershipBoardViewController.swift
+++ LeaderboardProject/Scenes/LeadershipBoardScreen/LeadershipBoardViewController.swift
@@ -4,7 +4,21 @@ class LeadershipBoardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        
+        addBackgroundGradientLayer()
     }
 }
 
+// MARK: Private methods
+
+private extension LeadershipBoardViewController {
+    func addBackgroundGradientLayer() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.gradientTop,
+                                UIColor.gradientCenter,
+                                UIColor.gradientBottom].map { $0.cgColor }
+        gradientLayer.locations = [0.0, 0.5, 1.0]
+        gradientLayer.frame = self.view.bounds
+        self.view.layer.insertSublayer(gradientLayer, at:0)
+    }
+}
